180d1e12899f2ed3c171c60a79537e0940f950e7
hovandien2004 <hovandien1812@gmail.com>
2026-01-22 21:33:15 +0700

    hvdein

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,18 +1,45 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Khai báo chân LED RGB
+#define RED_PIN   18
+#define GREEN_PIN 19
+#define BLUE_PIN  23
+
+// Hàm điều khiển RGB
+void rgb(int r, int g, int b) {
+  digitalWrite(RED_PIN,   r ? HIGH : LOW);
+  digitalWrite(GREEN_PIN, g ? HIGH : LOW);
+  digitalWrite(BLUE_PIN,  b ? HIGH : LOW);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  rgb(1, 0, 0); // Red
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  rgb(0, 1, 0); // Green
+  delay(1000);
+
+  rgb(0, 0, 1); // Blue
+  delay(1000);
+
+  rgb(1, 1, 0); // Yellow
+  delay(1000);
+
+  rgb(0, 1, 1); // Cyan
+  delay(1000);
+
+  rgb(1, 0, 1); // Magenta
+  delay(1000);
+
+  rgb(1, 1, 1); // White
+  delay(1000);
+
+  rgb(0, 0, 0); // OFF
+  delay(1000);
 }
